c72f100867c3246ff003867e0f68531a64047dfa
stephan281094 <stephan281094@gmail.com>
2016-05-09 20:29:53 +0200

    Make use of redux universal example

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,32 @@
 var path = require('path')
 var webpack = require('webpack')
-var autoprefixer = require('autoprefixer')
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'inline-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
+    'webpack-hot-middleware/client',
+    './src/client/index.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
-        include: path.join(__dirname, 'src')
-      },
-      { test: /\.css$/,
-        loader: 'style-loader!style-loader!css-loader!postcss-loader'
-      },
-      { test: /\.png$/,
-        loader: 'url-loader?limit=100000'
-      },
-      { test: /\.jpg$/,
-        loader: 'file-loader'
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          presets: [ 'react-hmre' ]
+        }
       }
     ]
-  },
-  postcss: function () {
-    return [autoprefixer]
   }
 }
