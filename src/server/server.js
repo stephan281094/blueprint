7c452d76114d601ca4915fbd51cf232a3ef51825
stephan281094 <stephan281094@gmail.com>
2016-05-11 18:36:30 +0200

    Clean up and improve accessibility

--- src/server/server.js
+++ src/server/server.js
@@ -51,7 +51,9 @@ function renderFullPage(html, initialState) {
     <!doctype html>
     <html>
       <head>
-        <title>Redux Universal Example</title>
+        <meta charset="utf-8">
+        <title>Blueprint</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </head>
       <body>
         <div id="root">${html}</div>
