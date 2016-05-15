5100698e36916ad7df723ebdbc599f8cf35bea9d
stephan281094 <stephan281094@gmail.com>
2016-05-15 21:40:03 +0200

    Add not found page

---
+++ src/common/containers/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Ooops!</h1>
+        <p>The page you requested for could not be found.</p>
+      </div>
+    )
+  }
+}
+
+export default NotFound
--- src/common/routes.js
+++ src/common/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import Home from './containers/Home'
+import NotFound from './containers/NotFound'
 
 const routes = (
   <Route path="/" component={App} >
     <IndexRoute component={Home} />
+    <Route path="*" component={NotFound} />
   </Route>
 )
 
