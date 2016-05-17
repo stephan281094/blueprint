418ba3d35d31cd4e16ba9a6d99df83e94bb40913
stephan281094 <stephan281094@gmail.com>
2016-05-17 09:01:53 +0200

    Add LanguageFiltered page

---
+++ src/common/containers/LanguageFiltered.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class LanguageFiltered extends Component {
+  render () {
+    return (
+      <h1>Language filtered page</h1>
+    )
+  }
+}
+
+export default LanguageFiltered
--- src/common/routes.js
+++ src/common/routes.js
@@ -4,12 +4,14 @@ import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import Home from './containers/Home'
 import Language from './containers/Language'
+import LanguageFiltered from './containers/LanguageFiltered'
 import NotFound from './containers/NotFound'
 
 const routes = (
   <Route path='/' component={App} >
     <IndexRoute component={Home} />
     <Route path=':language' component={Language} />
+    <Route path=':language/:type' component={LanguageFiltered} />
     <Route path='*' component={NotFound} />
   </Route>
 )
