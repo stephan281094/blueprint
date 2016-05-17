24a13fb7841c7d66155b363f472f233f6f2108fa
stephan281094 <stephan281094@gmail.com>
2016-05-17 08:59:25 +0200

    Add Language page

---
+++ src/common/containers/Language.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Language extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Language Page</h1>
+        <p>Showcasing all blueprints created for a specific language</p>
+      </div>
+    )
+  }
+}
+
+export default Language
--- src/common/routes.js
+++ src/common/routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
+
 import App from './containers/App'
 import Home from './containers/Home'
+import Language from './containers/Language'
 import NotFound from './containers/NotFound'
 
 const routes = (
   <Route path='/' component={App} >
     <IndexRoute component={Home} />
+    <Route path=':language' component={Language} />
     <Route path='*' component={NotFound} />
   </Route>
 )
