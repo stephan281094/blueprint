be1b2a695dbc1aa17aabee5206b1cc2afc0ea49f
stephan281094 <stephan281094@gmail.com>
2016-05-22 23:27:53 +0200

    Add redux logger

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
 export default function configureStore (initialState) {
+  const logger = createLogger()
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   )
 
   if (module.hot) {
