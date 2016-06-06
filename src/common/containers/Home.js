0e4a3072cb38f84389d8d30af71d42aa3a6f7eae
stephan281094 <stephan281094@gmail.com>
2016-06-06 23:52:21 +0200

    Improve modal styling

--- src/common/containers/Home.js
+++ src/common/containers/Home.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router'
 import Modal from 'react-modal'
 
 import GeneratorContainer from '../components/GeneratorContainer'
+import Icon from '../components/Icon'
+import NewBlueprintModal from '../components/modals/NewBlueprint'
 import Popular from '../components/Popular'
 import { setLanguageOption } from '../reducers/languages'
 import { setTypeOption } from '../reducers/types'
@@ -57,17 +59,10 @@ class Home extends Component {
           onRequestClose={this.closeModal.bind(this)}
           overlayClassName='modal-overlay'
           className='modal'>
-          <h1 className='title'>Modal title</h1>
-          <p className='title-sub'>With some description</p>
-          <p style={{maxWidth: 500}}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <button onClick={this.closeModal.bind(this)}>Close modal</button>
+          <NewBlueprintModal />
+          <button className='modal__button' onClick={this.closeModal.bind(this)}>
+            <Icon className='modal__button__icon' icon='multiply-big' />
+          </button>
         </Modal>
         <h1 className='title'>Blueprints</h1>
         <span className='title-sub'>
